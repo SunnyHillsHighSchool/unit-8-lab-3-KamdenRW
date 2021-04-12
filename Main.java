5908ff2eddf0dbaa33057aec2ecc1f92cc7241ff
Kamden Wright <>
2021-04-12 21:21:47 +0000

    Create a sepia tone method

---
+++ Main.java
@@ -0,0 +1,12 @@
+//Name: Kamden Wright
+
+class Main 
+{ 
+  public static void main (String []args)
+  {
+    Picture pic = new Picture("iphone.jpg");
+    //pic.//methodName;
+    pic.sepiaTone();
+    pic.write("output.jpg");
+  }
+}
